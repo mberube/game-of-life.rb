391db571546b337b84545c1b8637cabf3b248827
Mathieu Berube <mathieu.berube@gmail.com>
2011-05-26 16:31:12 -0400

    clarifications + simplifications

--- lib/RuleEngine.rb
+++ lib/RuleEngine.rb
@@ -1,16 +1,16 @@
 require 'Position'
 class RuleEngine
 	def next_cell_live?(position, board)
-		neighbors_count = find_neighbors_count(position, board)
+		neighbors_count = find_live_neighbors_count(position, board)
 		return board.alive?(position) if neighbors_count == 2
-		neighbors_count >= 2 && neighbors_count <= 3
+		neighbors_count == 3
 	end
 
-	def find_neighbors_count(position, board)
+	def find_live_neighbors_count(position, board)
 		count = 0
 		neighbors = Neighborhood.new.neighbors([position])
 		neighbors.each do |neighbor|
-		count = count + 1 if board.alive?(neighbor)
+			count = count + 1 if board.alive?(neighbor)
 		end
 		count
 	end
